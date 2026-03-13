4cc4682c1f1dd1d2394114e139c80368af32d268
augus <august.soeltoft@gmail.com>
2026-03-13 16:12:09 +0100

    Lidt opdelende kommentarer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,25 +9,31 @@ public class Main {
         Random random = new Random();
 
 
+        //Bottle Queue
         ReentrantLock bottleLock = new ReentrantLock(true);
         BottleQueue bottleQueue = new BottleQueue(bottleLock, bottleLock.newCondition(), bottleLock.newCondition(), new ArrayDeque<>());
 
+        //BottleProducer
         Thread producerThread = new Thread(new BottleProducerThread(bottleQueue, counter, random));
         producerThread.start();
 
+        //Beer Queue
         ReentrantLock beerLock = new ReentrantLock(true);
         BeerBottleQueue beerQueue = new BeerBottleQueue(beerLock, beerLock.newCondition(), beerLock.newCondition(), new ArrayDeque<>());
 
+        //Water Queue
         ReentrantLock waterLock = new ReentrantLock(true);
         WaterBottleQueue waterQueue = new WaterBottleQueue(waterLock, waterLock.newCondition(), waterLock.newCondition(), new ArrayDeque<>());
 
+        //Bottle Splitter
         Thread splitterThread = new Thread(new BottleSplitterThread(bottleQueue, beerQueue, waterQueue));
         splitterThread.start();
 
-
+        //Beer Consumer
         Thread beerConsumer = new Thread(new BeerConsumerThread(beerQueue));
         beerConsumer.start();
 
+        //Water Consumer
         Thread waterConsumer = new Thread(new WaterConsumerThread(waterQueue));
         waterConsumer.start();
     }
